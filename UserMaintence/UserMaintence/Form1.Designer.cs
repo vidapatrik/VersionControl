b0f46175ecb24f5efee0dd5dd4ad0c6b01e28f00
Patrik Vida <patrik.vida@stud.uni-corvinus.hu>
2022-10-01 14:27:10 +0200

    gomb hozzáadás mentés

--- UserMaintence/UserMaintence/Form1.Designer.cs
+++ UserMaintence/UserMaintence/Form1.Designer.cs
@@ -71,6 +71,7 @@ namespace UserMaintence
             this.button1.TabIndex = 3;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
--- UserMaintence/UserMaintence/Form1.cs
+++ UserMaintence/UserMaintence/Form1.cs
@@ -7,17 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintence.Entities;
 
 namespace UserMaintence
 {
     public partial class Form1 : Form
     {
-        public Form1()
+	
+       BindingList<Class1> users = new BindingList<Class1>(); 
+
+public Form1()
         {
             InitializeComponent();
             label1.Text = Resource1.LastName;
             label2.Text = Resource1.FirstName;
             button1.Text = Resource1.Add;
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new Class1()
+            {
+                LastName = label1.Text,
+                FirstName = label2.Text
+            };
+            users.Add(u);
         }
     }
 }
