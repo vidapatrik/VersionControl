aa7e104a668f1dda95af9fafd90a567a60b91aec
Patrik Vida <patrik.vida@stud.uni-corvinus.hu>
2022-10-07 19:38:53 +0200

    kesz

--- UserMaintence/UserMaintence/Form1.Designer.cs
+++ UserMaintence/UserMaintence/Form1.Designer.cs
@@ -35,6 +35,7 @@ namespace UserMaintence
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -87,11 +88,22 @@ namespace UserMaintence
             this.textBox2.Size = new System.Drawing.Size(266, 20);
             this.textBox2.TabIndex = 5;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(428, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(266, 109);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -113,6 +125,7 @@ namespace UserMaintence
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- UserMaintence/UserMaintence/Form1.cs
+++ UserMaintence/UserMaintence/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,15 +14,15 @@ namespace UserMaintence
 {
     public partial class Form1 : Form
     {
-	
-       BindingList<Class1> users = new BindingList<Class1>(); 
 
-public Form1()
+        BindingList<Class1> users = new BindingList<Class1>();
+
+        public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource1.LastName;
-            label2.Text = Resource1.FirstName;
-            button1.Text = Resource1.Add;
+            label1.Text = Resource1.FullName;
+            button2.Text = Resource1.button2;
+            button1.Text = Resource1.button1;
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
             listBox1.DisplayMember = "FullName";
@@ -31,10 +32,28 @@ public Form1()
         {
             var u = new Class1()
             {
-                LastName = label1.Text,
-                FirstName = label2.Text
+                FullName = label1.Text+label2.Text,
+
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK) //!a StreamWriter villanyos!!
+            {
+                StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8);
+                foreach (Class1 s in users)
+                {
+                    sw.Write(s.ID);
+                    sw.Write(';');
+                    sw.Write(s.FullName);
+                }
+
+                sw.Close();
+
+            }
         }
     }
 }
